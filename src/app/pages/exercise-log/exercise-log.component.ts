c517db4c13b6385345a7f092a152ebf0bc07c6c2
Genesis Carrero <genesis.carrero@autogeneral.com.au>
2023-06-07 13:27:56 +1000

    created a query param to pass the name of selected exercise

--- src/app/pages/exercise-log/exercise-log.component.ts
+++ src/app/pages/exercise-log/exercise-log.component.ts
@@ -1,48 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { ExerciseService, ExerciseEntry } from '../../services/exercise.service';
-
-
+import {
+    ExerciseService,
+    ExerciseEntry,
+} from '../../services/exercise.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-exercise-log',
-  templateUrl: './exercise-log.component.html',
-  styleUrls: ['./exercise-log.component.scss'],
-  
+    selector: 'app-exercise-log',
+    templateUrl: './exercise-log.component.html',
+    styleUrls: ['./exercise-log.component.scss'],
 })
+export class ExerciseLogComponent implements OnInit {
+    exerciseEntries: ExerciseEntry[] = [];
+    newExerciseEntry!: FormGroup;
 
+    constructor(
+        private route: ActivatedRoute,
+        private exerciseService: ExerciseService
+    ) {
+        console.log('hello there');
+    }
 
-export class ExerciseLogComponent {
+    ngOnInit(): void {
+        console.log('hello');
+        this.newExerciseEntry = this.getNewExerciseEntry();
+        this.route.queryParams.subscribe((params) => {
+            console.log(params);
 
-   constructor ( private exerciseService: ExerciseService) {}
+            if (params['exercise']) {
+                this.newExerciseEntry.patchValue({
+                    exercise: params['exercise'],
+                });
+            }
+        });
+    }
 
-  exerciseEntries: ExerciseEntry[] = [];
-  newExerciseEntry: FormGroup = this.getNewExerciseEntry();
+    addExerciseEntry(): void {
+        const newEntry: ExerciseEntry = this.newExerciseEntry.getRawValue();
+        this.exerciseEntries.push(newEntry);
+        this.exerciseService.saveExercises(this.exerciseEntries);
+        this.newExerciseEntry = this.getNewExerciseEntry();
+    }
 
-  addExerciseEntry(): void {
-    const newEntry: ExerciseEntry = this.newExerciseEntry.getRawValue();
-
-  
-    this.exerciseEntries.push(newEntry);
-
-    this.exerciseService.saveExercises(this.exerciseEntries)
-  
-    this.newExerciseEntry = this.getNewExerciseEntry();
-    // this.newExerciseEntry.reset();
-  }
-
-  getNewExerciseEntry(){
-    return new FormGroup({
-      exercise: new FormControl(''),
-      sets: new FormControl(0),
-      repetitions: new FormControl(0),
-    });
-
-    // return {
-    //   exercise: '',
-    //   sets: 0,
-    //   repetitions: 0
-    // }
-    // return a form group here 
-  }
+    getNewExerciseEntry(): FormGroup {
+        return new FormGroup({
+            exercise: new FormControl(''),
+            sets: new FormControl(0),
+            repetitions: new FormControl(0),
+        });
+    }
 }
--- src/app/pages/workout-page/workout-page.component.ts
+++ src/app/pages/workout-page/workout-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-workout-page',
-  templateUrl: './workout-page.component.html',
-  styleUrls: ['./workout-page.component.scss']
+    selector: 'app-workout-page',
+    templateUrl: './workout-page.component.html',
+    styleUrls: ['./workout-page.component.scss'],
 })
 export class WorkoutPageComponent {
+    items: string[] = ['Squats', 'Deadlifts', 'Bicepcurls', 'Bench Press'];
+    selectedExercise: string = '';
+    router: any;
 
-  items: string[] = ['Squats', 'Deadlifts', 'Bicepcurls', 'Bench Press'];
+    selectExercise(item: string): void {
+        this.selectedExercise = item;
+    }
 
+    navigateToExerciseLog(exercise: string) {
+        this.router.navigate(['/exercise-log'], { queryParams: { exercise } });
+    }
 }
