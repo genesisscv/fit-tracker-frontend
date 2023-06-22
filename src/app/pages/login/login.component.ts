644870b934747b9d189f04a374d62e068310f0c1
Genesis Carrero <genesis.carrero@autogeneral.com.au>
2023-06-22 13:10:57 +1000

    added fs package

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,7 +8,6 @@ import { Observable, lastValueFrom, throwError } from 'rxjs';
 // import { SessionStorageService } from '../../services/session/session-storage.service';
 import { AppUser } from '../../services/app/app.models';
 
-
 @Component({
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
@@ -24,20 +23,22 @@ export class LoginComponent {
     async onFormSubmit(loginDetails: LoginFormInterface) {
         try {
             this.loginRequest = await lastValueFrom(
-                this.loginService.login(loginDetails.userName, loginDetails.password).pipe(
-                    tap(() => {
-                        this.router.navigate(['../dashboard'], { relativeTo: this.activatedRoute });
-                    }),
-                    catchError((error) => {
-                        // Handle the error here
-                        console.error('Login failed:', error);
-                        // You can perform additional error handling, such as displaying an error message
-                        return throwError(error);
-                    })
-                )
+                this.loginService
+                    .login(loginDetails.userName, loginDetails.password)
+                    .pipe(
+                        tap(() => {
+                            this.router.navigate(['../dashboard'], {
+                                relativeTo: this.activatedRoute,
+                            });
+                        }),
+                        catchError((error) => {
+                            // Handle the error here
+                            console.error('Login failed:', error);
+                            // You can perform additional error handling, such as displaying an error message
+                            return throwError(error);
+                        })
+                    )
             );
-            
-
         } catch (error) {
             // Handle any errors thrown during the login process
             console.error('Login failed:', error);
